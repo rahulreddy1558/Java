3e9083dc6fb475bdaaff40af7dae0f42a49f609c
rahulreddy1558 <rahulreddy1558@gmail.com>
2018-10-14 20:23:45 -0700

    Linked List to array

---
+++ Java8Collections/src/Arrayaslist.java
@@ -0,0 +1,29 @@
+import java.util.LinkedList;
+import java.util.Arrays;
+
+
+public class Arrayaslist {
+
+	public static void main(String[] args) {
+		String[] a = {"red","blue","black","brown"};
+//		LinkedList<String>  k = (LinkedList<String>) Arrays.asList(a);
+		LinkedList<String>  k = new LinkedList<>(Arrays.asList(a));
+		k.add("pink");
+		k.add(2,"burgande");
+		k.addFirst("a");
+		k.addLast("z");
+		for(String b : k) {
+			System.out.println(b);
+		}
+		
+		/**
+		 * Printing LinkedList to array
+		 */
+		String[] p =  k.toArray(new String[k.size()]);
+		for(String b : p) {
+			System.out.println(b);
+		} 
+		
+	}
+
+}
