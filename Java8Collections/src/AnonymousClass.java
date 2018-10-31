23e5ecfd9160d645e47eb4e81ea3dd083b34e815
rahulreddy1558 <rahulreddy1558@gmail.com>
2018-10-30 21:46:22 -0700

    Inner class

---
+++ Java8Collections/src/AnonymousClass.java
@@ -0,0 +1,11 @@
+
+public class AnonymousClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Topclass a = new Topclass();
+		a.getVal1();
+		a.setVal1(20);
+	}
+}
+ 
