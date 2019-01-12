b96f0a5dbb9077ce6860c2f90dcc590dcb8f3713
rahulreddy1558 <rahulreddy1558@gmail.com>
2019-01-12 09:04:28 -0800

    Stack

---
+++ Java8/src/Collections/Stackexample.java
@@ -0,0 +1,13 @@
+package Collections;
+
+public class Stackexample {
+    public static void main(String[] args) {
+
+        Stack b = new Stack();
+        b.push(3);
+        System.out.println(b.tos);
+        b.pop();
+        System.out.println(b.tos);
+    }
+
+}
