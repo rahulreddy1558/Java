fb587649a710625a542ecd7ab26fe84981034de6
rahulreddy1558 <rahulreddy1558@gmail.com>
2019-01-12 09:00:39 -0800

    Stack

---
+++ Java8/src/Collections/Stack.java
@@ -0,0 +1,34 @@
+package Collections;
+
+public class Stack {
+    int[] a = new int[10];
+    int tos;
+
+    Stack(){
+
+        tos = -1;
+    }
+
+
+    void push(int item){
+        if(item < 0 | item > 10){
+            System.out.print("item supplied is out of range");
+        }
+        else {
+            a[++tos] = item;
+        }
+
+    }
+
+
+    int pop(){
+        if (tos < 0){
+            System.out.print("Out of stack");
+            return 0;
+        }
+        else {
+            return a[tos--];
+        }
+    }
+
+}
